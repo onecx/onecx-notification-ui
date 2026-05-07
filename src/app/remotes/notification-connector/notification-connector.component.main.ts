f864ec406987d262f41ea0d582c752aee35f7a83
Benjamin Pabst <166110368+BenjaminPabst@users.noreply.github.com>
2026-05-07 11:42:54 +0200

    fix: linting issues

--- src/app/remotes/notification-connector/notification-connector.component.main.ts
+++ src/app/remotes/notification-connector/notification-connector.component.main.ts
@@ -1,4 +1,2 @@
-/* eslint-disable prettier/prettier */
 ;(globalThis as typeof globalThis & { global?: typeof globalThis }).global ??= globalThis
-// eslint-disable-next-line @typescript-eslint/prefer-top-level-await
 import('./notification-connector.component.bootstrap').catch((err) => console.error(err))
