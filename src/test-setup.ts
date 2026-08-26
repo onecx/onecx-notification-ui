44084336fcf13cf0c75781d7bbc7d8ff34754587
priyankaSandeepRade <245361893+priyankaSandeepRade@users.noreply.github.com>
2026-08-26 13:37:00 +0530

    chore(deps): update dependancies to their patched version for fix security issues (#29)
    * fix: update js-yaml versions
    
    * fix: minimatch version
    
    * fix: build

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -5,4 +5,20 @@ globalThis.ngJest = {
     errorOnUnknownProperties: true
   }
 }
+
+Object.defineProperty(globalThis, 'matchMedia', {
+  writable: true,
+  value: (query: string): MediaQueryList =>
+    ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => undefined,
+      removeListener: () => undefined,
+      addEventListener: () => undefined,
+      removeEventListener: () => undefined,
+      dispatchEvent: () => false
+    }) as MediaQueryList
+})
+
 import 'jest-preset-angular/setup-jest'
