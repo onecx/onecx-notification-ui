35a2138bf1455fa1fe8df53d5b16b755b66a9417
SylwiaKruszynska1 <221315718+SylwiaKruszynska1@users.noreply.github.com>
2026-09-21 08:23:59 +0200

    fix: removed reference to non-existent file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StandaloneShellModule } from '@onecx/angular-standalone-shell'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
   imports: [StandaloneShellModule]
 })
 export class AppComponent {}
